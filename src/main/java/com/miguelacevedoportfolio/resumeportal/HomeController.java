284a898ce813a0e1e746e214a4b9f35e2050b300
Miguel Acevedo <44342378+miguelalb@users.noreply.github.com>
2021-09-02 08:25:22 -0400

    Initial commit - User JPA Infrastructure

---
+++ src/main/java/com/miguelacevedoportfolio/resumeportal/HomeController.java
@@ -0,0 +1,13 @@
+package com.miguelacevedoportfolio.resumeportal;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeController {
+
+    @GetMapping("/")
+    public String home() {
+        return "Hello Miguel!";
+    }
+}
